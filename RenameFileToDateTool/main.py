2c1e6e906608e8f52393d151d7756a385191c2fd
Daniel Marshall <hunuadan@gmail.com>
2024-01-26 15:29:50 +1300

    adds working functionality for videos and date modified field

--- RenameFileToDateTool/main.py
+++ RenameFileToDateTool/main.py
@@ -24,9 +24,14 @@ def rename_photos(directory, date_option, date_option_dict, exe, custom_filename
     # Counter for the number of files renamed
     files_renamed_count = 0
 
-    for file in files:
+    new_file_names = {}
 
-        process = subprocess.Popen([exe, file], stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
+    for file in files:
+        file_path = directory + "/" + file
+        print(file)
+        print(directory)
+        print(file_path)
+        process = subprocess.Popen([exe, file_path], stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
                                    universal_newlines=True)
         file_metadata = {}
         for output in process.stdout:
@@ -66,9 +71,12 @@ def rename_photos(directory, date_option, date_option_dict, exe, custom_filename
         else:
             new_file_name_base = date_time.strftime('%Y-%m-%d %H.%M')
 
-        
+        if new_file_name_base not in new_file_names:
+            new_file_names[new_file_name_base] = 0
+        new_file_names[new_file_name_base] += 1
+
         # Append the count and file extension to the new filename
-        new_file_name = f"{new_file_name_base}_{files_renamed_count + 1}.{file_metadata['File Extension']}"
+        new_file_name = f"{new_file_name_base}_{new_file_names[new_file_name_base]}.{file_metadata['File Type Extension']}"
 
         # Create the full path for the new file
         new_path = os.path.join(directory, new_file_name)
@@ -97,6 +105,21 @@ if __name__ == "__main__":
 
     if date_option == "cf":
         custom_filename = input("Choose a custom filename for renaming: ")
+
+
+    # filename = "2019-03-30 17-41-02.jpeg"
+    # filename = "20200216_074818_IMG_9669.JPG"
+    # date_time_string = filename.strip().rsplit('.')[0]
+    # print('date_time_string:', date_time_string)
+    # try:
+    #     date_time = parser.parse(date_time_string)
+    # except ValueError:
+    #     print(f"Error parsing datetime from filename: {filename}. Skipping.")
+    #
+    # print('date_time:', date_time)
+    #
+    # print(date_time.strftime('%Y-%m-%d %H.%M'))
+
     # file = "C:/Users/hunua/Downloads/gpt trial/by media created tag/2024-01-26 00.07_4.MOV"
     #file = "C:/Users/hunua/Downloads/gpt trial/2000 - 2010 - 1.jpg"
     # file = "C:/Users/hunua/Downloads/gpt trial/date taken/IMG_1593 (3).JPG"
